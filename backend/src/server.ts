708d576aa81f2c3bb76cc7c7de4f2480c2bb47a7
jasondansie <jasondansie@gmail.com>
2023-04-03 15:12:32 +0300

    adds cotroller, model and routes for surveys

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,7 @@ import connectDB from "./db/connect";
 import userRouter from "./routes/user";
 import questionRouter from "./routes/questions";
 import responsesRouter from "./routes/responses";
+import surveyRouter from "./routes/surveys";
 
 //middleware
 import notFoundMiddleware from "./middleware/notFound";
@@ -25,8 +26,9 @@ app.get("/api/v1", (req: Request, res: Response) => {
   res.json({ msg: "API" });
 });
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/questions", questionRouter);
 app.use("/api/v1/responses", responsesRouter);
+app.use("/api/v1/surveys", surveyRouter);
+app.use("/api/v1/questions", questionRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
