81477816beaa17627b82284cade525be89d2ded2
Eyvaz Alishov <110826244+CodeLaMat@users.noreply.github.com>
2023-04-24 19:12:13 +0300

    global start and install set up

--- frontend/src/components/login/Login.tsx
+++ frontend/src/components/login/Login.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { useAppDispatch} from "../../hooks/hooks";
+import { useAppDispatch } from "../../hooks/hooks";
 import classes from "./Login.module.css";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -12,29 +12,32 @@ import {
   setUserEmail,
   setUserData,
 } from "../../features/login/loginSlice";
-import { loginAsync, ldspLoginAsync, User } from "../../features/login/loginSlice";
+import {
+  loginAsync,
+  ldspLoginAsync,
+  User,
+} from "../../features/login/loginSlice";
 
 interface LoginProps {}
 
 const Login: React.FC<LoginProps> = () => {
-  
   const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-
   const loginHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     try {
-      await dispatch(ldspLoginAsync({ username: userName, password: password }));
+      await dispatch(
+        ldspLoginAsync({ username: userName, password: password })
+      );
       const token = sessionStorage.getItem("token");
       if (token) {
         try {
           const decodedToken: { [key: string]: any } = jwt_decode(token!);
           const userRole = decodedToken.user.role;
           const userEmail = decodedToken.user.email;
-
           const userData = Object.values(decodedToken) as User[];
           dispatch(setUserData(userData));
 
@@ -51,7 +54,7 @@ const Login: React.FC<LoginProps> = () => {
           dispatch(setIsAuthenticated(true));
           dispatch(setUserEmail(userEmail));
           dispatch(setSelectedRole(userRole));
-          
+
           navigate("/");
         } catch (error) {
           console.error(error);
