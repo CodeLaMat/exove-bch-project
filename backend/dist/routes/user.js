71322a03b843eb6b8dbdd1c811b54def73711871
Fuh Suh <tofuhsuh@gmail.com>
2023-04-26 19:35:38 +0300

    update ldap controllers and routes

--- backend/dist/routes/user.js
+++ backend/dist/routes/user.js
@@ -5,9 +5,11 @@ const router = (0, express_1.Router)();
 const user_1 = require("../controllers/user");
 const authentication_1 = require("../middleware/authentication");
 router.route("/auth/login").post(user_1.login);
+router.route("/auth/ldaplogin").post(user_1.ldapLogin);
 router
     .route("/user")
     .get(authentication_1.authenticateUser, (0, authentication_1.authorizePermissions)("hr"), user_1.getAllUsers);
+router.route("/ldapusers").get(user_1.getAllLdapUsers);
 router.route("/user/:id").get(authentication_1.authenticateUser, user_1.getOneUser);
 router.route("/showMe").get(authentication_1.authenticateUser, user_1.showCurrentUser);
 router.route("/auth/logout").get(user_1.logout);
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 const router = Router();
 import {
   login,
+  ldapLogin,
   getAllUsers,
+  getAllLdapUsers,
   getOneUser,
   logout,
   showCurrentUser,
@@ -12,9 +14,11 @@ import {
   authorizePermissions,
 } from "../middleware/authentication";
 router.route("/auth/login").post(login);
+router.route("/auth/ldaplogin").post(ldapLogin);
 router
   .route("/user")
   .get(authenticateUser, authorizePermissions("hr"), getAllUsers);
+router.route("/ldapusers").get(getAllLdapUsers);
 router.route("/user/:id").get(authenticateUser, getOneUser);
 router.route("/showMe").get(authenticateUser, showCurrentUser);
 router.route("/auth/logout").get(logout);
