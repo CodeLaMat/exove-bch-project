fe37866a9dae0fb88a86bcce534272ade795a33e
Fuh Suh <tofuhsuh@gmail.com>
2023-04-27 12:23:43 +0300

    updated controllers

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -8,7 +8,6 @@ import {
 } from "../errors";
 import { StatusCodes } from "http-status-codes";
 import jwt from "jsonwebtoken";
-
 // import bcrypt from "bcryptjs";
 import {
   attachCookiesToResponse,
@@ -34,7 +33,7 @@ interface LdapLoginRequestBody {
 }
 
 interface JwtPayload {
-  userId: string;
+  _id: string;
   email: string;
   role: string;
 }
@@ -61,25 +60,6 @@ const createNewSearchClient = () => {
   return client;
 };
 
-const createNewClient = () => {
-  const client = ldap.createClient({
-    url: 'ldap://localhost:389',
-  });
-
-  return client;
-};
-
-const createNewSearchClient = () => {
-  const client = ldap.createClient({
-    url: 'ldap://localhost:389',
-    bindDN: 'cn=admin,dc=test,dc=com', // add the admin account DN here
-    bindCredentials: 'myadminpassword' // add the admin account password here
-  });
-
-  return client;
-};
-
-
 const login = async (req: Request, res: Response) => {
   const { email, password }: LoginRequestBody = req.body;
   if (!email || !password) {
@@ -96,7 +76,7 @@ const login = async (req: Request, res: Response) => {
 
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError("Invalid password");
+    throw new UnauthenticatedError("Invalid Credentials");
   }
 
   // const tokenUser = {
@@ -125,31 +105,10 @@ const login = async (req: Request, res: Response) => {
   //   signed: true,
   // });
 
-
-  const payload: JwtPayload = {
-    userId: user._id,
-    email: user.email,
-    role: user.role,
-  };
-  const token = jwt.sign(payload, `${process.env.JWT_SECRET}`, {
-    expiresIn: `${process.env.JWT_LIFETIME}`,
-  });
-
-  console.log('token', token);
-
-  const oneDay = 1000 * 60 * 60 * 24;
-  res.cookie("token", token, {
-    httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
-    secure: process.env.NODE_ENV === "production",
-    signed: true,
-  });
-  // attachCookiesToResponse({ res, user: tokenUser });
   res.status(StatusCodes.OK).json({
-    user: token,
+    user: tokenUser,
   });
 };
-
 
 const logout = async (req: Request, res: Response) => {
   res.cookie("token", "logout", {
@@ -200,10 +159,8 @@ const getAllUsers = async (req: Request, res: Response) => {
 const ldapLogin = async (req: Request, res: Response) => {
   const { username, password }: LdapLoginRequestBody = req.body;
 
-
   console.log(`${username} is trying to login with ${password} as a pwd`);
   const client = createNewClient();
-
 
   const bindDN = `uid=${username},ou=People,dc=test,dc=com`;
 
@@ -357,145 +314,6 @@ const getAllLdapUsers = async (req: Request, res: Response) => {
   console.log("client unbound");
 };
 
-
-const ldapLogin = async (req: Request, res: Response) => {
-  const { username, password }: LdapLoginRequestBody = req.body;
-
-  console.log(`${username} is trying to login with ${password} as a pwd`);
-  const client = createNewClient();
-
-  const bindDN = `uid=${username},ou=People,dc=test,dc=com`;
-
-  client.bind(bindDN, password, (err: Error | null) => {
-    if (err) {
-      console.error(err);
-      res.status(401).send('Authentication failed');
-      return;
-    }
-  })
-
-  const searchOptions: SearchOptions = {
-    scope: 'sub',
-    filter: `(&(uid=${username})(objectClass=posixAccount))`, // add objectClass filter
-    attributes: ['cn', 'memberOf', 'gidNumber', 'description', 'mail', 'jpegPhoto', 'telephoneNumber' ],
-  };
-
-  client.search(`uid=${username},ou=People,dc=test,dc=com`, searchOptions, (err: Error | null, result: ldap.SearchCallbackResponse) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Error retrieving user info');
-      return;
-    }
-
-    const userAttributes: SearchEntryObject[] = [];
-
-    result.on('searchEntry', (entry) => {
-      const user: Record<string, any> = {};
-      entry.attributes.forEach((attribute) => {
-        const key = attribute.type;
-        const value = attribute.vals;
-        user[key] = value;
-      });
-      userAttributes.push(user as SearchEntryObject);
-    });
-
-    result.on('end', () => {
-      console.log("authentication successfull");
-      const userData = userAttributes[0];
-
-      const payload = { 
-        user: { 
-          role: userData.description,
-          name: userData.cn,
-          email: userData.mail,
-          phoneNumber: userData.telephoneNumber,
-          groupId: userData.gidNumber,
-          imagePath: userData.jpegPhoto,
-        } as LdapUser,
-      };
-      console.log("payload", payload);
-      const token = jwt.sign(payload, `${process.env.JWT_SECRET}`, {
-        expiresIn: "2d",
-      });
-
-      console.log("userToken", token);
-   
-        res.status(200).send({
-          message: 'Authentication successful',
-          user: userAttributes[0],
-          groups: userAttributes[0].memberOf, // get groups the user is a member of
-          token: token,
-        });
-    });
-  });
-}
-
-const getAllLdapUsers = async (req: Request, res: Response) => {
-  console.log("getting all ldap users");
-  const client = createNewClient();
-
-  const bindDN = `cn=admin,dc=test,dc=com`;
-
-  client.bind(bindDN, "myadminpassword", (err: Error | null) => {
-    if (err) {
-      console.error(err);
-      res.status(401).send('Authentication failed');
-      return;
-    }
-  })
-
-    const opts: SearchOptions = {
-      filter: '(objectClass=inetOrgPerson)',
-      scope: 'sub',
-      attributes: ['*'],
-    };
-
-    const users: any[] = [];
-
-    client.search(`ou=People,dc=test,dc=com`, opts, (err: Error | null, result: ldap.SearchCallbackResponse) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error retrieving user info');
-        return;
-      }
-  
-      const userAttributes: SearchEntryObject[] = [];
-  
-      result.on('searchEntry', (entry) => {
-        const user: Record<string, any> = {};
-        entry.attributes.forEach((attribute) => {
-          const key = attribute.type;
-          const value = attribute.vals;
-          user[key] = value;
-        });
-        userAttributes.push(user as SearchEntryObject);
-      });
-  
-      result.on('end', () => {
-        console.log("authentication successfull");
-        const userData = users[0];
-  
-        const payload = { 
-          user: { 
-            role: userData.description,
-            name: userData.cn,
-            email: userData.mail,
-            phoneNumber: userData.telephoneNumber,
-            groupId: userData.gidNumber,
-            imagePath: userData.jpegPhoto,
-          } as LdapUser,
-        };
-        console.log("payload", payload);
-      });
-      
-    });
-
-    await client.unbind();
-    console.log("client unbound");
-
-}
-
-
 const getOneUser = async (req: Request, res: Response) => {
   const {
     params: { id: userId },
@@ -530,4 +348,3 @@ export {
   logout,
   showCurrentUser,
 };
-
