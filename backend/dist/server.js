ea9af4f02ea13377029a0e47ba0010cbe9551365
Fuh Suh <tofuhsuh@gmail.com>
2023-05-02 11:44:02 +0300

    update server.ts

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -25,7 +25,7 @@ const errorHandler_1 = __importDefault(require("./middleware/errorHandler"));
 const PORT = process.env.PORT || 5010;
 app.use((0, morgan_1.default)("tiny"));
 app.use((0, cors_1.default)({
-    origin: "http://localhost:3001",
+    origin: "http://localhost:3000",
     credentials: true, // enable credentials for all endpoints
 }));
 app.use(express_1.default.urlencoded({ extended: false }));
