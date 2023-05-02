bac46a5e446db936a25acf1f0ba62448643d595d
Fuh Suh <tofuhsuh@gmail.com>
2023-05-02 11:44:02 +0300

    update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5010;
 app.use(morgan("tiny"));
 app.use(
   cors({
-    origin: "http://localhost:3001", // replace with the origin of your client application
+    origin: "http://localhost:3000", // replace with the origin of your client application
     credentials: true, // enable credentials for all endpoints
   })
 );
